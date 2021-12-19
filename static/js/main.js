ebd9c3c0f4ec591ea421d7e27f338086840d2700
XhmikosR <xhmikosr@gmail.com>
2021-12-19 21:34:39 +0200

    Remove unused Contributor Card code

--- static/js/main.js
+++ static/js/main.js
@@ -34,9 +34,9 @@
     langPickerElement.removeChild(currentLangElement.parentNode);
   }
 
-  const toggleFunction = function () {
+  var toggleFunction = function () {
     langPickerElement.classList.toggle('hidden');
-    const isAriaExpanded =
+    var isAriaExpanded =
       langPickerTogglerElement.getAttribute('aria-expanded') === 'true';
     langPickerTogglerElement.setAttribute('aria-expanded', !isAriaExpanded);
   };
@@ -54,8 +54,8 @@
     }
   });
 })();
+
 (function () {
-  const themeAttr = 'data-theme';
   var darkThemeSwitcherElement = document.querySelector('.dark-theme-switcher');
 
   darkThemeSwitcherElement.addEventListener('click', function () {
@@ -68,7 +68,7 @@
   });
 
   function setTheme(theme) {
-    document.querySelector('html').setAttribute(themeAttr, theme);
+    document.documentElement.setAttribute('data-theme', theme);
     window.localStorage.setItem('theme', theme);
   }
 
@@ -76,6 +76,7 @@
     return window.localStorage.getItem('theme');
   }
 })();
+
 (function () {
   var scrollToTop = document.querySelector('#scroll-to-top');
 
@@ -91,158 +92,10 @@
     window.scrollTo(0, 0);
   });
 })();
-(function () {
-  var contributorCard = document.querySelector('.contributor-card');
 
-  if (!contributorCard) {
-    return;
-  }
-
-  var contributorAvatar = contributorCard.querySelector('#contributor-avatar');
-  var contributorUsername = contributorCard.querySelector(
-    '#contributor-username'
-  );
-  var contributorContributions = contributorCard.querySelector(
-    '#contributor-contributions'
-  );
-  var loadingSpinner = contributorCard.querySelector('.spinner-border');
-
-  if (window.IntersectionObserver) {
-    var observer = new window.IntersectionObserver(
-      function (entries) {
-        entries.forEach(function (entry) {
-          if (entry.intersectionRatio > 0.5) {
-            // In viewport, fetch a random contributor
-            fetchRandomContributor();
-
-            observer.unobserve(entry.target);
-          }
-        });
-      },
-      { threshold: 0.5 }
-    );
-
-    observer.observe(document.querySelector('footer'));
-  } else {
-    // Does not support IntersectionObserver
-    fetchRandomContributor();
-  }
-
-  function fetchRandomContributor() {
-    var maxContributors;
-    var fetchDate;
-    var needToRefetch = false;
-
-    if (window.localStorage) {
-      maxContributors = window.localStorage.getItem('max_contributors');
-      fetchDate = parseInt(window.localStorage.getItem('fetch_date'), 10);
-    }
-
-    // If fetch date is a month old (2592000000 ms === 30 days)
-    if (Date.now() - fetchDate >= 2592000000) {
-      needToRefetch = true;
-    }
-
-    // If localStorage and data is less than 1 month old, fetch 1 time
-    if (maxContributors && !needToRefetch) {
-      getContributor(
-        Math.floor(Math.random() * Math.floor(parseInt(maxContributors))) + 1
-      );
-    } else {
-      getMaxContributors(function (randomPage, lastPage) {
-        getContributor(randomPage);
-
-        if (window.localStorage) {
-          window.localStorage.setItem('max_contributors', lastPage);
-        }
-      });
-    }
-  }
-
-  function getMaxContributors(callback) {
-    var xhr = new window.XMLHttpRequest();
-    xhr.open(
-      'GET',
-      'https://api.github.com/repos/nodejs/node/contributors?per_page=1',
-      true
-    );
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          // Get Headers Links last page to generate a random contributor
-          var links = linkParser(xhr.getResponseHeader('Link'));
-          var randomPage =
-            Math.floor(
-              Math.random() * Math.floor(parseInt(links.last.page, 10))
-            ) + 1;
-
-          if (window.localStorage) {
-            window.localStorage.setItem('fetch_date', Date.now());
-          }
-          callback(randomPage, links.last.page);
-        } else {
-          return contributorCard.parentNode.removeChild(contributorCard);
-        }
-      }
-    };
-
-    xhr.send();
-  }
-
-  function getContributor(randomPage) {
-    var xhr = new window.XMLHttpRequest();
-    xhr.open(
-      'GET',
-      'https://api.github.com/repos/nodejs/node/contributors?per_page=1&page=' +
-        randomPage,
-      true
-    );
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          var contributor = JSON.parse(xhr.responseText)[0];
-
-          // Remove loading spinner and show avatar
-          loadingSpinner.parentNode.removeChild(loadingSpinner);
-          contributorAvatar.classList.remove('hidden');
-          // Set new values
-          contributorAvatar.src = contributor.avatar_url + '&s=80';
-          contributorAvatar.parentElement.href = contributor.html_url;
-          contributorUsername.textContent = contributor.login;
-          contributorUsername.href = contributor.html_url;
-          contributorContributions.textContent =
-            contributor.contributions + ' contributions';
-          contributorContributions.parentElement.href =
-            'https://github.com/nodejs/node/commits?author=' +
-            contributor.login;
-        } else {
-          return contributorCard.parentNode.removeChild(contributorCard);
-        }
-      }
-    };
-
-    xhr.send();
-  }
-
-  function linkParser(linkHeader) {
-    var regex = /<([^?]+\?per_page=1&[a-z]+=([\d]+))>;[\s]*rel="([a-z]+)"/g;
-    var array = [];
-    var object = {};
-
-    while ((array = regex.exec(linkHeader)) !== null) {
-      object[array[3]] = {
-        url: array[1],
-        page: array[2]
-      };
-    }
-
-    return object;
-  }
-})();
 (function () {
   'use strict';
+
   var userAgent = navigator.userAgent;
   var osMatch = userAgent.match(/(Win|Mac|Linux)/);
   var os = (osMatch && osMatch[1]) || '';
@@ -298,10 +151,9 @@
     winText.textContent = winText.textContent.replace(/x(86|64)/, arch);
   }
 })();
-(function () {
-  // This function is used to replace the anchor
-  // link of Edit on GitHub
 
+// This function is used to replace the anchor link of Edit on GitHub
+(function () {
   var editOnGitHubElement = document.getElementById('editOnGitHubLink');
   var editOnGitHubUrlElement = document.getElementById('editOnGitHubUrl');
 
